d36511984122f01be6381ee8119c860e42aea155
TheBest125 <kevinbruyne0125@gmail.com>
2023-10-07 16:22:38 +0530

    made a simple player

---
+++ entities/player.py
@@ -0,0 +1,32 @@
+import pygame
+from constants.constants import SCREEN_WIDTH,SCREEN_HEIGHT
+from pygame.locals import (
+K_w,
+K_a,
+K_s,
+K_d,
+K_j,
+)
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+    def update(self, pressed_keys):
+        if pressed_keys[K_w]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_s]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_a]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_d]:
+            self.rect.move_ip(5, 0)
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
